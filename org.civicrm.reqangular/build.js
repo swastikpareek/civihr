82550ed478fd3fb0bb49c34cf4cca79ac6df6b6d
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-02-05 16:04:26 +0100

    (Reqangular) Configure bower

--- org.civicrm.reqangular/build.js
+++ org.civicrm.reqangular/build.js
@@ -1,80 +1,80 @@
 ({
-    baseUrl : 'src',
-    out: 'dist/reqangular.min.js',
-    uglify: {
-        no_mangle: true,
-        max_line_length: 1000
+  baseUrl : 'src',
+  out: 'dist/reqangular.min.js',
+  uglify: {
+    no_mangle: true,
+    max_line_length: 1000
+  },
+  paths: {
+    'common/angular': 'common/vendor/angular/angular',
+    'common/angularAnimate': 'common/vendor/angular-animate/angular-animate',
+    'common/angularBootstrap': 'common/vendor/angular-bootstrap/ui-bootstrap',
+    'common/angularFileUpload': 'common/vendor/angular-file-upload/angular-file-upload',
+    'common/angularMocks': 'common/vendor/angular-mocks/angular-mocks',
+    'common/angularResource': 'common/vendor/angular-resource/angular-resource',
+    'common/angularRoute': 'common/vendor/angular-route/angular-route',
+    'common/angularUiRouter': 'common/vendor/angular-ui-router/release/angular-ui-router',
+    'common/angularXeditable': 'common/vendor/angular-xeditable/dist/js/xeditable',
+    'common/d3': 'common/vendor/d3/d3',
+    'common/lodash': 'common/vendor/lodash/lodash',
+    'common/moment': 'common/vendor/moment/min/moment.min',
+    'common/rangy-core': 'common/vendor-custom/rangy-core',
+    'common/rangy-selectionsaverestore': 'common/vendor-custom/rangy-selectionsaverestore',
+    'common/require': 'common/vendor/requirejs/require',
+    'common/text-angular': 'common/vendor-custom/textAngular',
+    'common/text-angular-sanitize': 'common/vendor/textAngular/src/textAngular-sanitize',
+    'common/text-angular-setup': 'common/vendor/textAngular/src/textAngularSetup',
+    'common/ui-select': 'common/vendor/ui-select/dist/select',
+    'common/vendor/perfect-scrollbar': 'common/vendor/perfect-scrollbar/js/perfect-scrollbar',
+    'common/mocks': '../test/mocks'
+  },
+  shim: {
+    'common/angular': {
+      exports: 'angular'
     },
-    paths: {
-        'common/angular': 'common/vendor/angular/angular.min',
-        'common/angularAnimate': 'common/vendor/angular/angular-animate.min',
-        'common/angularBootstrap': 'common/vendor/angular/ui-bootstrap',
-        'common/angularFileUpload': 'common/vendor/angular/angular-file-upload',
-        'common/angularMocks': 'common/vendor/angular/angular-mocks',
-        'common/angularResource': 'common/vendor/angular/angular-resource.min',
-        'common/angularRoute': 'common/vendor/angular/angular-route.min',
-        'common/angularUiRouter': 'common/vendor/angular/angular-ui-router.min',
-        'common/angularXeditable': 'common/vendor/angular/xeditable.min',
-        'common/rangy-core': 'common/vendor/angular/rangy-core',
-        'common/rangy-selectionsaverestore': 'common/vendor/angular/rangy-selectionsaverestore',
-        'common/text-angular-sanitize': 'common/vendor/angular/textAngular-sanitize.min',
-        'common/text-angular-setup': 'common/vendor/angular/textAngularSetup',
-        'common/text-angular': 'common/vendor/angular/textAngular',
-        'common/ui-select': 'common/vendor/angular/select',
-        'common/require': 'common/vendor/require.min',
-        'common/d3': 'common/vendor/d3.min',
-        'common/lodash': 'common/vendor/lodash.min',
-        'common/moment': 'common/vendor/moment.min',
-        'common/mocks': '../test/mocks',
-        'common/vendor/perfect-scrollbar': 'common/vendor/perfect-scrollbar.min'
+    'common/angularAnimate': {
+      deps: ['common/angular']
     },
-    shim: {
-        'common/angular': {
-            exports: 'angular'
-        },
-        'common/angularAnimate': {
-            deps: ['common/angular']
-        },
-        'common/angularBootstrap': {
-            deps: ['common/angular']
-        },
-        'common/angularMocks': {
-          deps: ['common/angular']
-        },
-        'common/angularResource': {
-            deps: ['common/angular']
-        },
-        'common/angularRoute': {
-            deps: ['common/angular']
-        },
-        'common/angularUiRouter': {
-            deps: ['common/angular']
-        },
-        'common/angularXeditable': {
-            deps: ['common/angular']
-        },
-        'common/text-angular': {
-            deps: [
-                'common/rangy-core',
-                'common/rangy-selectionsaverestore',
-                'common/text-angular-sanitize',
-                'common/text-angular-setup'
-            ]
-        },
-        'common/ui-select': {
-            deps: ['common/angular']
-        }
+    'common/angularBootstrap': {
+      deps: ['common/angular']
     },
-    include: [
-        'common/bundles/vendors',
-        'common/bundles/apis',
-        'common/bundles/services',
-        'common/bundles/directives',
-        'common/bundles/angular-date',
-        'common/bundles/routers',
-        'common/bundles/models',
-        'common/modules/dialog',
-        'common/modules/templates',
-        'common/modules/xeditable-civi'
-    ]
+    'common/angularMocks': {
+      deps: ['common/angular']
+    },
+    'common/angularResource': {
+      deps: ['common/angular']
+    },
+    'common/angularRoute': {
+      deps: ['common/angular']
+    },
+    'common/angularUiRouter': {
+      deps: ['common/angular']
+    },
+    'common/angularXeditable': {
+      deps: ['common/angular']
+    },
+    'common/text-angular': {
+      deps: [
+        'common/rangy-core',
+        'common/rangy-selectionsaverestore',
+        'common/text-angular-sanitize',
+        'common/text-angular-setup'
+      ]
+    },
+    'common/ui-select': {
+      deps: ['common/angular']
+    }
+  },
+  include: [
+    'common/bundles/vendors',
+    'common/bundles/apis',
+    'common/bundles/services',
+    'common/bundles/directives',
+    'common/bundles/angular-date',
+    'common/bundles/routers',
+    'common/bundles/models',
+    'common/modules/dialog',
+    'common/modules/templates',
+    'common/modules/xeditable-civi'
+  ]
 })
--- org.civicrm.reqangular/test/test-main.js
+++ org.civicrm.reqangular/test/test-main.js
@@ -5,67 +5,67 @@ var mocksPath = extPath + '/test/mocks';
 var srcPath = extPath + '/src/common';
 
 Object.keys(window.__karma__.files).forEach(function(file) {
-    if (TEST_REGEXP.test(file)) {
-        allTestFiles.push(file);
-    }
+  if (TEST_REGEXP.test(file)) {
+    allTestFiles.push(file);
+  }
 });
 
 require.config({
-    deps: allTestFiles,
-    waitSeconds: 60,
-    shim: {
-        'common/angular': {
-            exports: 'angular'
-        },
-        'common/angularAnimate': {
-            deps: ['common/angular']
-        },
-        'common/angular-date': {
-            deps: ['common/angular']
-        },
-        'common/angularBootstrap': {
-            deps: ['common/angular']
-        },
-        'common/angularMocks': {
-            deps: ['common/angular']
-        },
-        'common/angularResource': {
-            deps: ['common/angular']
-        },
-        'common/angularRoute': {
-            deps: ['common/angular']
-        },
-        'common/angularUiRouter': {
-            deps: ['common/angular']
-        },
-        'common/angularUiRouter': {
-            deps: ['common/angular']
-        },
-        'common/ui-select': {
-            deps: [
-              'common/angular',
-              'common/text-angular-sanitize'
-            ]
-        }
+  deps: allTestFiles,
+  waitSeconds: 60,
+  shim: {
+    'common/angular': {
+      exports: 'angular'
     },
-    paths: {
-        'common': srcPath,
-        'common/mocks': mocksPath,
-        'common/angular': srcPath + '/vendor/angular/angular.min',
-        'common/moment': srcPath + '/vendor/moment.min',
-        'common/angularRoute': srcPath + '/vendor/angular/angular-route.min',
-        'common/angularUiRouter': srcPath + '/vendor/angular/angular-ui-router.min',
-        'common/angularMocks': srcPath + '/vendor/angular/angular-mocks',
-        'common/angularBootstrap': srcPath + '/vendor/angular/ui-bootstrap',
-        'common/lodash': srcPath + '/vendor/lodash.min',
-        'common/ui-select': srcPath + '/vendor/angular/select',
-        'common/vendor/perfect-scrollbar': srcPath + '/vendor/perfect-scrollbar.min',
-        'common/text-angular-sanitize': srcPath + '/vendor/angular/textAngular-sanitize.min'
+    'common/angularAnimate': {
+      deps: ['common/angular']
     },
-    callback: function () {
-        // Simple hack to provide value to CRM.vars.reqangular.baseURL
-        CRM.vars = { reqangular: { baseURL: extPath } };
-
-        window.__karma__.start();
+    'common/angular-date': {
+      deps: ['common/angular']
+    },
+    'common/angularBootstrap': {
+      deps: ['common/angular']
+    },
+    'common/angularMocks': {
+      deps: ['common/angular']
+    },
+    'common/angularResource': {
+      deps: ['common/angular']
+    },
+    'common/angularRoute': {
+      deps: ['common/angular']
+    },
+    'common/angularUiRouter': {
+      deps: ['common/angular']
+    },
+    'common/angularUiRouter': {
+      deps: ['common/angular']
+    },
+    'common/ui-select': {
+      deps: [
+        'common/angular',
+        'common/text-angular-sanitize'
+      ]
     }
+  },
+  paths: {
+    'common': srcPath,
+    'common/mocks': mocksPath,
+    'common/angular': srcPath + '/vendor/angular/angular.min',
+    'common/angularRoute': srcPath + '/vendor/angular-route/angular-route.min',
+    'common/angularUiRouter': srcPath + '/vendor/angular-ui-router/release/angular-ui-router.min',
+    'common/angularMocks': srcPath + '/vendor/angular-mocks/angular-mocks',
+    'common/angularBootstrap': srcPath + '/vendor/angular-bootstrap/ui-bootstrap.min',
+    'common/lodash': srcPath + '/vendor/lodash/lodash.min',
+    'common/moment': srcPath + '/vendor/moment/min/moment.min',
+    'common/text-angular-sanitize': srcPath + '/vendor/textAngular/dist/textAngular-sanitize.min',
+    'common/ui-select': srcPath + '/vendor/ui-select/dist/select.min',
+    'common/vendor/perfect-scrollbar': srcPath + '/vendor/perfect-scrollbar/js/perfect-scrollbar.min',
+  },
+  callback: function () {
+    // Simple hack to provide value to CRM.vars.reqangular.baseURL
+    CRM.vars = { reqangular: { baseURL: extPath } };
+
+    window.__karma__.start();
+  }
 });
